b18d5d3e1b41f8939e0fd638e186fcf521fe5114
defneelagoz <defne.elagoez@tum.de>
2026-01-14 23:34:42 +0100

    envrevert

--- predictive_model/dashboard2.py
+++ predictive_model/dashboard2.py
@@ -7,12 +7,6 @@ import matplotlib.pyplot as plt
 import os
 import sys
 import subprocess
-from dotenv import load_dotenv
-
-# Load environment variables
-# Path to .env file relative to this script: ../.env/.env
-env_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env', '.env')
-load_dotenv(dotenv_path=env_path)
 
 # Add current directory to path to import local modules
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
